6f9dd815dcdd302e2c66f70e2ae768822544ffaa
Les Hill <leshill@gmail.com>
2013-06-28 16:13:12 -0700

    Basic Rails 4 with handlebars_assets

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CheckV4::Application.config.secret_key_base = 'd3986f6643ec6db57cfe5d6e69c7084833002eddd96e0411a1a3b13a326c491e9b59e6572631405553d795beef237214fd1ea7690acd13a68beb00d8af8db23a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CheckV4::Application.config.session_store :cookie_store, key: '_check_v4_session'
